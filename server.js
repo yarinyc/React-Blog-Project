bc156e66502790ea583d8171c820a70f80d45c4c
omerdv <42119031+omerdv@users.noreply.github.com>
2020-02-18 20:09:32 +0200

    Add files via upload
    18.2

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const data = require ('./data.json');
+const data = require('./data.json');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use((_, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', '*');
@@ -16,11 +18,17 @@ app.use((_, res, next) => {
 });
 
 app.get('/api/posts', (req, res) => {
-  res.send(data);
+	res.send(data);
 });
 
 app.post('/api/posts', (req, res) => {
-  data.unshift(req.body);
+	data.unshift(req.body);
 });
+
+app.delete('/api/posts', (req, res) => {
+
+	// error here - req.body is not the post id for some reason (it is {})
+	data.splice(req.body, 1);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
